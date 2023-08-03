44b34b68efa1bd5f6725631c954fe5f0e0d3d60f
alessandro dominici <alessandrodominici@Mac-mini-de-alessandro.local>
2023-08-03 18:25:46 +0200

    jueves,  3 de agosto de 2023, 18:22:18 CEST nothing to be done

--- automatic_commit.sh
+++ automatic_commit.sh
@@ -4,6 +4,7 @@ function automatic_commit() {
 	comment=$(date)
 	echo -e "${RED}Proceeding with automatic_commit.${GREEN}\n"
 	if [[ -n "$status" ]]; then
+		$PWD > $traces
 		git add . > $traces
 		git commit -m "$comment" >> $traces
 		git push >> $traces
