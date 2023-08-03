64f02a6614bc4dbdefc2eba15487b212438da8b8
Gogiu23 <giuliandominici@gmail.com>
2023-08-03 18:53:01 +0200

    jueves,  3 de agosto de 2023, 18:53:01 CEST

--- manual_commit.sh
+++ manual_commit.sh
@@ -8,17 +8,20 @@ function manual_commit() {
 		echo -e "${CYAN}Do you want to stage and push theese unstages changes?\n${NC}"
 		echo -e "${YELLOW}(type ${BOLD}${RED}yes${NC}${YELLOW} or ${BOLD}${RED}no${NC}${YELLOW} or ${BOLD}${RED}diff${NC}${YELLOW} in case you wanna know the diffs)${MAGENTA} "
 		read answer
+		echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" > $traces
+		echo -e "All the changes manually should be recorded down here" >> $traces
+		echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
 
 		if [[ "$answer" == "diff" ]]; then
-			git diff 
-			git diff > $traces
-			exit
+			git diff >> $traces
 		fi
 		echo -e "${YELLOW}Still wanna stage and push? (yes or no) ${MAGENTA}" 
 		read answer
 		if [[ "$answer" == "yes" ]]; then
 			echo -e "${CYAN}Making commits...\n${NC}"
-			git add .
+			echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
+			git add . >> $traces
+			echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
 			echo -e "${BLUE}Wanna add something as commentary to your commit? (if not the date would be print instead)\n${NC}" 
 			echo -e "${YELLOW}(Write your comments or type no)${GREEN} " 
 			read comment
@@ -26,8 +29,11 @@ function manual_commit() {
 				comment=$(date)
 				echo "$comment"
 			fi
-			git commit -m "$comment"
-			git push
+			echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
+			git commit -m "$comment" >> $traces
+			echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
+			git push >> $traces
+			echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
 			echo -e "${NC}"
 			fi
 		fi
