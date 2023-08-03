d9fefc926d5926e531208389d116a66f4cffc937
Gogiu23 <giuliandominici@gmail.com>
2023-08-03 18:42:07 +0200

    jueves,  3 de agosto de 2023, 18:42:07 CEST

--- automatic_commit.sh
+++ automatic_commit.sh
@@ -4,7 +4,7 @@ function automatic_commit() {
 	comment=$(date)
 	echo -e "${RED}Proceeding with automatic_commit.${GREEN}\n"
 	if [[ -n "$status" ]]; then
-		echo "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" > $traces
+		echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" > $traces
 		echo -e "Here the direction you are making changes\n" >> $traces
 		echo -e "=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" >> $traces
 		echo -e "$PWD\n" >> $traces
