900cd5ef27861ea646f6f569d07f65c2df4cef18
Giuliano <giuliandominici@gmail.com>
2023-08-03 05:57:30 +0200

    jue 03 ago 2023 05:57:30 CEST

---
+++ Git.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Función para verificar si hay cambios sin hacer push en un repositorio de Git
+function check_git_status() {
+  # Obtener el estado del repositorio de Git
+  status=$(git status --porcelain)
+
+  # Comprobar si hay cambios sin hacer commit ni push
+  if [[ -n "$status" ]]; then
+    echo -e "Hay cambios sin hacer commit ni push en: $(pwd)\n"
+	read -p "Do you want to stage and push theese unstages changes? (type yes or no) " answer
+
+	if [ "$answer" == "yes" ]; then
+		echo -e "Making commits...\n"
+		git add .
+		git commit -m "$(date)"
+	fi
+  fi
+}
+
+# Bucle para recorrer las carpetas
+for dir in $(find . -type d -name '.git' -prune); do
+  # Entrar al directorio del repositorio de Git
+  pushd "$dir/.." > /dev/null
+
+  # Verificar el estado del repositorio
+  check_git_status
+
+  # Regresar al directorio anterior
+  popd > /dev/null
+done
+
