ac7992310c2dd5e151d3528cc349da5edda7a4a3
alessandro dominici <alessandrodominici@Mac-mini-de-alessandro.local>
2023-08-03 18:28:14 +0200

    some changes

--- automatic_commit.sh
+++ automatic_commit.sh
@@ -4,7 +4,7 @@ function automatic_commit() {
 	comment=$(date)
 	echo -e "${RED}Proceeding with automatic_commit.${GREEN}\n"
 	if [[ -n "$status" ]]; then
-		$PWD > $traces
+		echo "$PWD" > $traces
 		git add . > $traces
 		git commit -m "$comment" >> $traces
 		git push >> $traces
