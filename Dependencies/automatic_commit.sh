269cf9e9ba086b3d3d6d0973d8a847dd5b189887
Gogiu23 <giuliandominici@gmail.com>
2023-08-04 11:33:05 +0200

    viernes,  4 de agosto de 2023, 11:33:05 CEST

--- Dependencies/automatic_commit.sh
+++ Dependencies/automatic_commit.sh
@@ -2,7 +2,7 @@
 
 function automatic_commit() {
 	comment=$(date)
-	echo -ne "${RED}Proceeding with automatic_commit.${GREEN}\r"
+	echo -ne "\n${RED}Proceeding with automatic_commit.${GREEN}\r"
 	sleep 0.1
 	if [[ -n "$status" ]]; then
 		if [[ "$counter" == "0" ]]; then
