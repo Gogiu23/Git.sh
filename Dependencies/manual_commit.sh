31a4e94c60cb0f7be446b243eccbf06a15eb2b18
Gogiu23 <giuliandominici@gmail.com>
2023-08-04 15:26:13 +0200

    viernes,  4 de agosto de 2023, 15:26:13 CEST

--- Dependencies/manual_commit.sh
+++ Dependencies/manual_commit.sh
@@ -36,7 +36,7 @@ function manual_commit() {
 			echo -e "${CYAN}Wanna add something as commentary to your commit? (if not the date would be print instead)\n${NC}" 
 			echo -e "${YELLOW}(Write your comments or type no)${GREEN} " 
 			read comment
-			if [[ "$comment" == "no" ]]; then
+			if [[ !"$comment" ]]; then
 				comment=$(date)
 				echo "$comment"
 			fi
