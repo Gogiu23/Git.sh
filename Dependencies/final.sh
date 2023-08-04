d74f9aa14edc10aea78a1f9c8cc8c048684cae13
Gogiu23 <giuliandominici@gmail.com>
2023-08-04 18:16:19 +0200

    Fri Aug  4 18:16:19 CEST 2023

---
+++ Dependencies/final.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function final(){
+	echo -e "Would you like to install the program in your PC?"
+	echo -e "(yes) or (no)"
+	read fin_pwd
+
+	if [[ "$fin_pwd" == "yes" ]]; then
+	fi
+}
