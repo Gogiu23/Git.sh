347db23c7cc4c4d906a7effc1c2937860473f4d1
Giuliano <giuliandominici@gmail.com>
2023-08-12 03:15:53 +0200

    sáb 12 ago 2023 03:15:53 CEST

--- git.sh
+++ git.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
-source $HOME/Escritorio/Git.sh/Dependencies/welcome.sh
-source $HOME/Escritorio/Git.sh/Dependencies/check_git_status.sh
-source $HOME/Escritorio/Git.sh/Dependencies/manual_commit.sh
-source $HOME/Escritorio/Git.sh/Dependencies/automatic_commit.sh
-source $HOME/Escritorio/Git.sh/Dependencies/log.sh
-source $HOME/Escritorio/Git.sh/Dependencies/commits.sh
-source $HOME/Escritorio/Git.sh/Dependencies/Colores.sh
-source $HOME/Escritorio/Git.sh/Dependencies/Pull.sh
+dependency="$HOME/.config/git_sh"
+
+if [[ -d "$dependency" ]]; then
+	echo -e "The directory exist already. No change are made"
+	cp -r Dependencies "$dependency" 
+else
+	echo -n "The directory doesn't exist. Creating"
+	sleep 1
+	echo -n "."
+	sleep 1
+	echo -n "."
+	sleep 1
+	echo "."
+	mkdir "$dependency"
+	cp -r Dependencies "$dependency" 
+	if [[ $? -eq "0" ]]; then
+		echo -e "Directory succesfully created"
+	else
+		echo -e "Error, the directory couldn't be created"
+	fi
+fi
+
+bin_dir="$HOME/.localbin"
+if [[ -d "$bin_dir" ]]; then
+	echo -e "The directory exist already. No change are made"
+	cp git.sh "$bin_dir"
+else
+	echo -n "The directory doesn't exist. Creating"
+	sleep 1
+	echo -n "."
+	sleep 1
+	echo -n "."
+	sleep 1
+	echo  "."
+	mkdir "$bin_dir"
+	cp git.sh "$bin_dir"
+	if [[ $? -eq "0" ]]; then
+		echo -e "Directory succesfully created"
+	else
+		echo -e "Error, the directory couldn't be created"
+	fi
+fi
+
+source $dependency/Dependencies/welcome.sh
+source $dependency/Dependencies/check_git_status.sh
+source $dependency/Dependencies/manual_commit.sh
+source $dependency/Dependencies/automatic_commit.sh
+source $dependency/Dependencies/log.sh
+source $dependency/Dependencies/commits.sh
+source $dependency/Dependencies/Colores.sh
+source $dependency/Dependencies/Pull.sh
 
 welcome
 
